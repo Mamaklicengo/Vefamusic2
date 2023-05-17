f4f9c2e325c18256c11af11fcbff5c31f07d5b1c
Mamaklicengo <128487934+Mamaklicengo@users.noreply.github.com>
2023-05-17 21:13:32 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Mamaklicengo/Vefamusic2",
+    "https://github.com/Mamaklicengo/YukkiMusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
